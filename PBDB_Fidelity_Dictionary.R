172aff1a8edefd3548efca878001aeadaac6afc9
Erika Ito <etito@wisc.edu>
2016-09-29 12:31:24 -0500

    Update PBDB_Fidelity_Dictionary.R

--- PBDB_Fidelity_Dictionary.R
+++ PBDB_Fidelity_Dictionary.R
@@ -5,5 +5,4 @@ library(RCurl)
 UnitsURL<-paste("https://macrostrat.org/api/units?lith_class=sedimentary&environ_class=marine&project_id=1&response=long&format=csv")
 GotURL<-getURL(UnitsURL)
 UnitsFrame<-read.csv(text=GotURL,header=TRUE)
-# Extract actual unit names
-UnitsDictionary<-unique(UnitsFrame[,"unit_name"])
+
