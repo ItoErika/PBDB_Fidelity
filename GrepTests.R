b0be7c1a09d8cf0f75884a69b4eecfaefa2a680e
Erika Ito <etito@wisc.edu>
2016-10-12 16:13:50 -0500

    Update GrepTests.R

--- GrepTests.R
+++ GrepTests.R
@@ -81,25 +81,3 @@ PatternsFixedHits<-sapply(CandidateUnits[1:50], greplPatternsFixed, CleanedWords
                      
                      
                      
-Test<-grepl("Lexington Limestone",Sentences,fixed=TRUE) & grepl("fossil",Sentences,fixed=TRUE)                   
-                              
-function2<-function(CandidateUnits,CleanedWords){
-  Hits<-sapply(CandidateUnits[1:15, function(x,y) grep(x,y, ignore.case=FALSE, perl=TRUE, CleanedWords[1:5000])
-  return(Hits)
-  }    
-    
-    
-function2<-sapply(CandidateUnits[1:15, function(x,y) grepl(x,y, ignore.case=FALSE, fixed=TRUE, perl=TRUE),CleanedWords[1:5000]) 
-sapply(CandidateUnits[1:15],function(x,y) grepl(x,y,ignore.case=FALSE,fixed=TRUE),
-sapply(CandidateUnits[1:15],function(x,y) grepl(x,y,ignore.case=FALSE,fixed=TRUE),CleanedWords[1:5000]) & 
-grepl("fossil",CleanedWords[1:5000],fixed=TRUE) & grepl("fossiliferous",CleanedWords[1:5000]),
-sapply(CandidateUnits[1:15],function(x,y) grepl(x,y,ignore.case=FALSE,fixed=TRUE),CleanedWords[1:5000]) & 
-grepl("fossil",CleanedWords[1:5000],fixed=TRUE)
-
-  
-  
-sapply(LongUnitDictionary[1:15],function(x,y) grepl(x,y,ignore.case=FALSE,perl=TRUE),CleanedWords[1:5000]),
-sapply(LongUnitDictionary[1:15],function(x,y) grep(x,y,ignore.case=FALSE,fixed=TRUE,pearl=TRUE),CleanedWords[1:5000]), 
-sapply(LongUnitDictionary[1:15],function(x,y) grep(x,y,ignore.case=FALSE),CleanedWords[1:5000]),replications=20)
-
-Test<-grepl("Lexington Limestone",Sentences,fixed=TRUE) & grepl("fossil",Sentences,fixed=TRUE)
