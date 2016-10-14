5e0d965f6ad22cfa4adf1a48a00a84ae5b137a05
Erika Ito <etito@wisc.edu>
2016-10-14 15:46:17 -0500

    Update AccuracyTests.R

--- AccuracyTests.R
+++ AccuracyTests.R
@@ -3,3 +3,10 @@ SampleData<-FossilData[sample(c(1:nrow(FossilData)),100,replace=FALSE,prob=NULL)
 # Save SampleData to a folder
 write.csv(SampleData,file="~/Documents/DeepDive/PBDB_Fidelity/R/SampleFrame.csv",row.names=FALSE)
 # Open the csv in excel or libre office and perform a manual accuracy test
+
+# ACCURACY TEST AFTER ADDING NON CANDIDATE MACROSTRAT DICTIONARY SEARCH AND NOISY WORD REMOVAL.
+# Take a random sample of 100 FinalFossilData Rows to check accuracy
+SampleData<-FinalFossilData[sample(c(1:nrow(FinalFossilData)),100,replace=FALSE,prob=NULL),]
+# Save SampleData to a folder
+write.csv(SampleData,file="~/Documents/DeepDive/PBDB_Fidelity/R/SampleFrame2.csv",row.names=FALSE)
+# Open the csv in excel or libre office and perform a manual accuracy test
